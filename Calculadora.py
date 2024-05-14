b1d4c30bedccb8a0a54dbbdc67333342ada21399
JoaoBringmann <149213160+JoaoBringmann@users.noreply.github.com>
2024-05-14 19:49:22 -0300

    Funções do conjunto completo

--- Calculadora.py
+++ Calculadora.py
@@ -13,42 +13,35 @@ def diferenca_conjuntos(A, B):
     return A.difference(B)
 
 def verificar_subconjunto_proprio(A, B):
-    if A.issubset(B) and A != B:
+    if A.issubset(B) and A!= B:
         return True
     else:
         return False
 
-def resultado_conjuntos():
-    valores = valores_conjuntos()
-    print("União: ", uniao_conjuntos(valores[0], valores[1]))
-    print("Intersecção: ", interseccao_conjuntos(valores[0], valores[1]))
-    print("Diferença: ", diferenca_conjuntos(valores[0], valores[1]))
-    if verificar_subconjunto_proprio(valores[0], valores[1]):
-        print("A é um subconjunto próprio de B")
-    else:
-        print("A não é um subconjunto próprio de B")
+def resultado_conjuntos(calculo, A, B):
+    if calculo == "1":
+        print("União: ", uniao_conjuntos(A, B))
+    elif calculo == "2":
+        print("Intersecção: ", interseccao_conjuntos(A, B))
+    elif calculo == "3":
+        print("Diferença: ", diferenca_conjuntos(A, B))
+    elif calculo == "4":
+        if verificar_subconjunto_proprio(A, B):
+            print("A é um subconjunto próprio de B")
+        else:
+            print("A não é um subconjunto próprio de B")
 
 def menu():
     while True:
-        menu = input("O que deseja fazer?\nConjuntos numéricos\nFunções do segundo grau\nFunções exponenciais\nMatrizes\nSair\n").lower()
-        while menu not in ["Conjuntos numéricos", "Funções do segundo grau", "Funções exponenciais","Matrizes","Sair"]:
-                print("Desculpe, não entendi. Por favor, digite Conjuntos numéricos, Funções do segundo grau, Funções exponenciais, Matrizes, Sair.")
-                menu = input("O que deseja fazer?\nConjuntos numéricos\nFunções do segundo grau\nFunções exponenciais\nMatrizes\nSair\n").lower()
-        if menu == "conjuntos numéricos":
-            calculo = input("O que você deseja fazer?\nUnião de conjuntos\nIntersecção de conjuntos\nDiferença de conjuntos\nVerificação de subconjunto Próprio\n").lower()
-            if calculo == "união de conjuntos":
-                resultado_conjuntos()
-            elif calculo == "intersecção de conjuntos":
-                resultado_conjuntos()
-            elif calculo == "diferença de conjuntos":
-                resultado_conjuntos()
-            elif calculo == "verificação de subconjunto próprio":
-                resultado_conjuntos()
-            else:
-                print("Digite Novamente")
-
-        elif menu == "sair":
-            print("Fechando")
+        menu = input("O que deseja fazer?\n1) Conjuntos numéricos\n2)Funções do segundo grau\n3) Funções exponenciais\n4) Matrizes\n5) Sair\n")
+        if menu == "5":
+            print("Fechando...")
             break
+        elif menu == "1":
+            valores = valores_conjuntos()
+            calculo = input("O que você deseja fazer?\n1) União de conjuntos\n2) Intersecção de conjuntos\n3) Diferença de conjuntos\n4) Verificação de subconjunto Próprio\n")
+            resultado_conjuntos(calculo, valores[0], valores[1])
+        else:
+            print("Desculpe, não entendi. Por favor, digite: \n1) Conjuntos numéricos\n2)Funções do segundo grau\n3) Funções exponenciais\n4) Matrizes\n5) Sair\n")
 
 menu()
